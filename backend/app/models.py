50ce4da376d72fc201ead1e5924b9283e60f0014
bhardwajprince09 <bhardwajprince0904@gmail.com>
2025-08-28 17:45:37 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List
 
+
+class POI(BaseModel):
+id: str
+name: str
+city: str
+lat: float
+lon: float
+tags: List[str]
+price: str
+rating: float
